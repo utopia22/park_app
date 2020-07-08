fc14ed45ceb6285f1c6585eb15677960202041cf
sekishinya <sekishinya@example.com>
2020-07-08 21:50:54 +0900

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,10 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root "posts#index"
-  resources :posts, only: [:index, :new, :create, :show, :edit, :update]
+  devise_for :users
+  resources :posts
 
-  devise_for :users, :controllers => {
-   :registrations => 'users/registrations',
-   :sessions => 'users/sessions',
-   :passwords => 'users/passwords'
-  }
+  resources :users, only: :show
 
   devise_scope :user do
     get 'my_page' => 'users/registrations#my_page'
