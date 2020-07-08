bb039e3ef3c16e654d1aad0e363bbd544fd8a90c
sekishinya <sekishinya@example.com>
2020-07-08 17:47:12 +0900

    ユーザー画像カラム追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 module ApplicationHelper
+  def avatar_url(user)
+    return user.image_name unless user.image_name.nil?
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "https://www.gravatar.com/avatar/#{gravatar_id}.jpg"
+  end
 
 end
