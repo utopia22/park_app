b5c2c57ed0f737ccea99b065573cb92a4135b417
sekishinya <sekishinya@example.com>
2020-07-12 17:15:54 +0900

    イイね機能未完

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
 
   mount_uploader :image_name, ImageUploader
+
+  has_many :likes, dependent: :destroy
+  has_many :like_posts, through: :likes, source: :post
 end
