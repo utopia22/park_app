75a1bb07253cc857d8038eded5eb4fbc5b5231ea
sekishinya <sekishinya@example.com>
2020-07-13 20:24:44 +0900

    一斉インデント修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,25 +3,25 @@ class LikesController < ApplicationController
 
    def index
      @like_post = current_user.like_posts
-  end
+   end
 
-  def create
-      like = Like.new
-      like.user_id = current_user.id
-      like.post_id = params[:post_id]
+   def create
+     like = Like.new
+     like.user_id = current_user.id
+     like.post_id = params[:post_id]
 
-      if like.save
-        redirect_to posts_path, success: 'いいねしました'
-      else
-        redirect_to posts_path, danger: 'いいねに失敗しました'
-      end
+     if like.save
+       redirect_to posts_path, success: 'いいねしました'
+     else
+       redirect_to posts_path, danger: 'いいねに失敗しました'
+     end
     end
 
 
-  def destroy
-      @like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
-      @like.destroy
-       redirect_to posts_path, success: 'いいねを取り消しました'
+     def destroy
+       @like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
+       @like.destroy
+      redirect_to posts_path, success: 'いいねを取り消しました'
   end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,39 +1,39 @@
 class PostsController < ApplicationController
   before_action :move_to_index, except: :index
 
-     def index
-        @posts = Post.all.order(created_at: :desc)
-      end
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
 
-      def new
-        @post = Post.new
-      end
+  def new
+    @post = Post.new
+  end
 
-      def create
-          Post.create(post_params)
+  def create
+    Post.create(post_params)
 
-          redirect_to :action => :index
-      end
+    redirect_to :action => :index
+   end
 
-      def show
-          @post = Post.find(params[:id])
-       end
+   def show
+     @post = Post.find(params[:id])
+   end
 
-       def edit
-        @post = Post.find(params[:id])
-    end
+   def edit
+     @post = Post.find(params[:id])
+   end
 
-    def update
-        post = Post.find(params[:id])
-        post.update(post_params)
-        redirect_to post
-    end
+   def update
+     post = Post.find(params[:id])
+     post.update(post_params)
+     redirect_to post
+   end
 
     def destroy
-        post = Post.find(params[:id])
-        post.delete
+      post = Post.find(params[:id])
+      post.delete
 
-        redirect_to posts_path
+      redirect_to posts_path
     end
 
     def move_to_index
@@ -41,9 +41,9 @@ class PostsController < ApplicationController
     end
 
 
-      private
+    private
 
-      def post_params
-          params.require(:post).permit(:image, :park, :outline, :location, :access)
-      end
+    def post_params
+      params.require(:post).permit(:image, :park, :outline, :location, :access)
+    end
 end
