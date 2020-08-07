a40c9a5c3b6ab148d0c1ed374af04b537e44d7a7
sekishinya <sekishinya@example.com>
2020-08-07 21:10:07 +0900

    user周り編集

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def avatar_url(user)
-    return user.image_name unless user.image_name.nil?
+    return image_name unless image_name.nil?
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     "https://www.gravatar.com/avatar/#{gravatar_id}.jpg"
   end
