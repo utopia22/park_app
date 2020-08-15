aa7aced319d3e54b519b0bf1034d33b5b31ecb75
sekishinya <sekishinya@example.com>
2020-08-15 15:34:44 +0900

    config.assets.js_to_production.rb

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,3 +3,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+config.assets.js_compressor = Uglifier.new(harmony: true)
