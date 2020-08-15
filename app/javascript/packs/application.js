3fc0e8b321974843e3f6e0d7339b48a6af57b093
sekishinya <sekishinya@example.com>
2020-08-15 14:43:22 +0900

    require(turbolinks).start()削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,26 +1,23 @@
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("bootstrap/dist/js/bootstrap")
 require('jquery')
 
 
-document.addEventListener('turbolinks:load', () => {
-    $(function () {
-        $('#search-park').keyup(function () {
-            const $railsText = $("#search-park").val().toLowerCase()
-            $('.main-contents').each(function () {
-                const val = $(this).text();
-                if (val.toLowerCase().includes($railsText)) {
-                    $(this).show();
-                } else {
-                    $(this).hide();
-                }
-            });
+$(function () {
+    $('#search-park').keyup(function () {
+        const $railsText = $("#search-park").val().toLowerCase()
+        $('.main-contents').each(function () {
+            const val = $(this).text();
+            if (val.toLowerCase().includes($railsText)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
         });
     });
-})
+});
 
 
 $(function(){
