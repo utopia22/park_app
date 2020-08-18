83c3648efd41734c951c089cc07e6eb24a353b43
sekishinya <sekishinya@example.com>
2020-08-18 19:33:41 +0900

    config/webpack/production.js戻し

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,1 +1,3 @@
-config.assets.js_compressor = Uglifier.new(harmony: true)
+process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+const environment = require('./environment')
+module.exports = environment.toWebpackConfig()
