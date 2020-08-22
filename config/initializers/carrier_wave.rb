2c512960416355d469b0d8349c636373a648dec1
sekishinya <sekishinya@example.com>
2020-08-23 02:54:10 +0900

    S3変数に代入

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,13 @@
-if Rails.env.production?
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-      region: Rails.application.credentials.dig(:aws, :region)
+      aws_access_key_id: Rails.application.credentials.dig(:aws, :AKIAVUQYNEDVNAK3NME5),
+      aws_secret_access_key: Rails.application.credentials.dig(:aws, :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),
+      region: Rails.application.credentials.dig(:aws, :ap-northeast-1)
     }
-    config.fog_directory = Rails.application.credentials.dig(:aws, :bucket)
+    config.fog_directory = Rails.application.credentials.dig(:aws, :little-park)
   end
-end
