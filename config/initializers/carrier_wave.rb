8fb3de07f082ce7c83e62ed7e786020650b8d111
sekishinya <sekishinya@example.com>
2020-08-23 10:19:29 +0900

    マスターキー

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,11 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
+if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.dig(:aws, :AKIAVUQYNEDVNAK3NME5),
-      aws_secret_access_key: Rails.application.credentials.dig(:aws, :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),
-      region: Rails.application.credentials.dig(:aws, :ap-northeast-1)
+      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+      region: Rails.application.credentials.dig(:aws, :region)
     }
-    config.fog_directory = Rails.application.credentials.dig(:aws, :little-park)
+    config.fog_directory = Rails.application.credentials.dig(:aws, :bucket)
   end
+end
