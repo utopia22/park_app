1737f25e4823bf6b38d6c09d07a8ef1252f834ab
sekishinya <sekishinya@example.com>
2020-08-25 19:53:15 +0900

    IAM再登録

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,11 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
 
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  storage :fog
 
   process convert: 'jpg'
 
