f50559dc2c5f3bbcd336821e84a5a130e22a01f7
sekishinya <sekishinya@example.com>
2020-08-26 20:10:17 +0900

    carrier_wave.rb変更

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,22 +3,22 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.production?
+  if Rails.env.production? || Rails.env.development?
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'littlepark'
-    config.asset_host = 'https://littlepark.s3.amazonaws.com'
+    config.asset_host = 'https://s3.amazonaws.com/littlepark'
+
+    config.fog_public = false # ←コレ
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION'],
-      path_style: true
+      aws_access_key_id: 'AWS_ACCESS_KEY_ID',
+      aws_secret_access_key: 'AWS_SECRET_ACCESS_KEY',
+      region: 'ap-northeast-1',
+
     }
   else
     config.storage :file
     config.enable_processing = false if Rails.env.test?
   end
 end
-
-CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
