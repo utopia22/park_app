38e0990bc52a3f3dd2c50e5e22b9425e95388ce1
sekishinya <sekishinya@example.com>
2020-08-26 20:59:08 +0900

    carrier_wave.rb正規に戻し

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,24 +1,11 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
-CarrierWave.configure do |config|
-  if Rails.env.production? || Rails.env.development?
-    config.storage :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'littlepark'
-    config.asset_host = 'https://s3.amazonaws.com/littlepark'
-
-    config.fog_public = false # ←コレ
+if Rails.env.production?
+  CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AWS_ACCESS_KEY_ID',
-      aws_secret_access_key: 'AWS_SECRET_ACCESS_KEY',
-      region: 'ap-northeast-1',
-
+      aws_access_key_id: ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET_KEY'],
+      region: ENV['S3_REGION']
     }
-  else
-    config.storage :file
-    config.enable_processing = false if Rails.env.test?
+    config.fog_directory = ENV['S3_BUCKET']
   end
 end
