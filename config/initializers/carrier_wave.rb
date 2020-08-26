6d21c4201362d76f28d62d44d0f69fb8efd5f416
sekishinya <sekishinya@example.com>
2020-08-26 19:58:11 +0900

    carrierwave.rb変更

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,24 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'littlepark'
+    config.asset_host = 'https://littlepark.s3.amazonaws.com'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-      region: Rails.application.credentials.dig(:aws, :region)
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION'],
+      path_style: true
     }
-    config.fog_directory = Rails.application.credentials.dig(:aws, :bucket)
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
 end
+
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
