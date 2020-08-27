8a3b878046a90f4bcd04bbe9cc6f6e7b6b81de64
sekishinya <sekishinya@example.com>
2020-08-27 20:51:45 +0900

    original_filename_to_image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,8 +7,6 @@ class ImageUploader < CarrierWave::Uploader::Base
     storage :file
   end
 
-  process convert: 'jpg'
-
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -22,13 +20,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   def filename
-    "#{secure_token}.#{file.extension}" if original_filename.present?
-  end
-
-  protected
-
-  def secure_token
-    var = :"@#{mounted_as}_secure_token"
-    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+    original_filename if original_filename
   end
 end
