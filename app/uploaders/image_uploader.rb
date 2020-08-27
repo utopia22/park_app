151f93031968075b81e0cbccd959659a23de7b49
sekishinya <sekishinya@example.com>
2020-08-27 22:36:24 +0900

    secure_token

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -18,5 +18,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   def extension_white_list
     %W[jpg jpeg gif png]
   end
-  
+
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
+
 end
