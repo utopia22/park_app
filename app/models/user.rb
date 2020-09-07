b482722960a14370becd981766405226638fe741
sekishinya <sekishinya@example.com>
2020-09-07 21:31:13 +0900

    コメントアウト消去

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,6 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(name: 'マーズギャ', email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
-      #user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
     end
   end
 
